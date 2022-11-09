6163897f296609a47c19256bcf41bb13e5924617
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-09 19:24:42 -0300

    style: adiciona o tema da aplicação

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from '../styles/global';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
