9ba77c3ed5662a032ad45c345aaa8a1ccc2b38e4
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-11 16:07:22 -0300

    feat: adiciona componente e configuração do next para imagens

--- components/MovieCard/index.jsx
+++ components/MovieCard/index.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+
 import { Container } from './styles';
 
+const imgPath = process.env.NEXT_PUBLIC_IMG_PATH;
+
 export default function MovieCard({ movie }) {
+  console.log(movie);
+
   return (
     <Container>
+      <Image
+        src={`${imgPath}${movie.poster_path}`}
+        alt="Poster do Filme"
+        width={189}
+        height={284}
+      />
+
       <p>{movie.title}</p>
     </Container>
   );
--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ['image.tmdb.org'],
+  },
 };
 
 module.exports = nextConfig;
