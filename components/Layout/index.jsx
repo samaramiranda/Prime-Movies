c9d19d6d68145b4201b2635f21fccb19ba6f2efd
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-11 13:16:31 -0300

    style: altera background das telas

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,12 +1,10 @@
 import Navbar from '../Navbar';
 
-import { Container } from './styles';
-
 export default function Layout({ children }) {
   return (
-    <Container>
+    <>
       <Navbar />
       <main>{children}</main>
-    </Container>
+    </>
   );
 }
--- styles/global.js
+++ styles/global.js
@@ -8,6 +8,15 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  body {
+    min-height: 100vh;
+    background: linear-gradient(
+      ${({ theme }) => theme.colors.richBlack} 50%,
+      ${({ theme }) => theme.colors.ultramarineBlueOpacity25}
+    );
+    background-color: ${({ theme }) => theme.colors.richBlack};
+  }
+
   a {
     color: inherit;
     text-decoration: none;
