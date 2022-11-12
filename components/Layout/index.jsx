dac398790d25c4b242a566fc839f82a295b61f45
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-12 20:43:39 -0300

    chore: adiciona o título da página

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+
 import Navbar from '../Navbar';
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Prime Movies</title>
+      </Head>
+
       <Navbar />
       <main>{children}</main>
     </>
