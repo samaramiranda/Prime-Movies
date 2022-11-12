e788ffddf82bff8c063cf07018eefeb346bf5607
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-12 20:21:55 -0300

    style: remove margin top da avaliação

--- components/MovieCard/Rating/styles.js
+++ components/MovieCard/Rating/styles.js
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   gap: 0.25rem;
-  margin-top: 0.1875rem;
   color: ${({ theme }) => theme.colors.goldenrod};
 
   p {
