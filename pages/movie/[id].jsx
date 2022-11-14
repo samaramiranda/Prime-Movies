522e15ff33909106a5e8adfebf88f2fb606ca132
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-14 13:37:23 -0300

    feat: adiciona requisição de detalhes do filme

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -1,5 +1,13 @@
-export default function Movie({ id }) {
-  return <h1>ID: {id}</h1>;
+import getMovies from '../../services/getMovies';
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const apiLanguage = process.env.NEXT_PUBLIC_API_LANGUAGE;
+const apiMovieDetailsPath = process.env.NEXT_PUBLIC_API_MOVIE_DETAILS_PATH;
+
+export default function Movie({ data }) {
+  console.log('data', data);
+
+  return <h1 style={{ color: '#fff' }}>ID: {data.title}</h1>;
 }
 
 export const getStaticPaths = async () => ({
@@ -16,9 +24,12 @@ export const getStaticPaths = async () => ({
 export const getStaticProps = async ({ params }) => {
   const { id } = params;
 
+  const movieUrl = `${apiMovieDetailsPath}${id}?api_key=${apiKey}&${apiLanguage}`;
+  const data = await getMovies(movieUrl);
+
   return {
     props: {
-      id,
+      data,
     },
     revalidate: 300,
   };
