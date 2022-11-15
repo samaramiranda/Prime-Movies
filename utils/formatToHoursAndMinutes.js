863a0676fc3be092c094839d8f35db84d2d62fe0
Samara Miranda <samaramiranda.13@hotmail.com>
2022-11-15 01:28:50 -0300

    fix: corrige retorno da função de formatar horas e minutos

--- utils/formatToHoursAndMinutes.js
+++ utils/formatToHoursAndMinutes.js
@@ -2,7 +2,7 @@ const formatToHoursAndMinutes = totalMinutes => {
   const hours = Math.floor(totalMinutes / 60);
   const minutes = totalMinutes % 60;
 
-  return `${hours}h ${minutes ? `${minutes}m` : ''}`;
+  return minutes ? `${hours}h ${minutes}m` : `${hours}h`;
 };
 
 export default formatToHoursAndMinutes;
